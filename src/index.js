f7cb303b4df8c72718dc22218754993a29a5df54
VIktor <afonkovviktor@yandex.ru>
2023-11-10 16:24:40 +0300

    update 4 step

--- src/index.js
+++ src/index.js
@@ -2,12 +2,57 @@ import _ from 'lodash';
 import { getPath, getData } from './parseJson.js';
 
 function getDifferentObject(obj1, obj2) {
-    const dataObj1 = JSON.stringify(obj1).split(' ');
-    const dataObj2 = JSON.stringify(obj2).split(' ');
-    const str = _.union([...dataObj1, ...dataObj2].flat());
-    const strClone = _.cloneDeep(str).sort();
-    return strClone.join(' ');;
-}
+    const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
+    const result = allKeys.map((key) => {
+        const oldValue = obj1[key];
+        const newValue = obj2[key];
+        if (!_.has(obj2, key)) {
+            return {
+                action: 'deleted',
+                key,
+                oldValue
+        }
+        };
+        if (!_.has(obj1, key)) {
+            return {
+                action: 'added',
+                key,
+                newValue
+            };
+        };
+        if (oldValue !== newValue) {
+            return {
+                action: 'changed',
+                key,
+                oldValue,
+                newValue
+            };
+        };
+        return {
+            action: 'unchanged',
+            key,
+            oldValue
+        };
+    })
+    .map((item) => {
+        const result = [];
+        if (item.action === 'deleted') {
+            result.push(`- ${item.key}: ${item.oldValue}`);
+            }
+        if (item.action === 'unchanged') {
+            result.push(`  ${item.key}: ${item.oldValue}`);
+            }
+        if (item.action === 'changed') {
+            result.push(`- ${item.key}: ${item.oldValue}`);
+            result.push(`+ ${item.key}: ${item.newValue}`);
+            }
+        if (item.action === 'added') {
+            result.push(`+ ${item.key}: ${item.newValue}`);
+            }
+            return result;
+        });
+    return result.flat();
+};
 
 function genDiff(filepath1, filepath2) {
     const dataFile1 = getData(getPath(filepath1));
--- src/parseJson.js
+++ src/parseJson.js
@@ -2,62 +2,94 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
-const filepath1 = path.resolve('__fixtures__/file1.json');
-const obj1 = JSON.parse(fs.readFileSync(filepath1));
-console.log(obj1)
-// const JSONobj1 = JSON.stringify(obj1)
-// console.log(JSONobj1)
-const filepath2 = path.resolve('/Users/vik/Documents/GitHub/frontend-project-46/__fixtures__/file2.json');
-const obj2 = JSON.parse(fs.readFileSync(filepath2));
-console.log(obj2)
-// const JSONobj2 = JSON.stringify(obj2)
-// console.log(JSONobj2)
-
 function getPath(str) {
-    return str.startsWith("/") ? path.resolve(str) : process.cwd(str);
-}
+    if (str.startsWith("/")) {
+        return path.resolve(str);
+    } else {
+        const dirName = process.cwd(str);
+        return path.resolve(dirName, str);
+    };
+};
 
 function getData(obj) {
     return JSON.parse(fs.readFileSync(obj))
 }
 
-function genDifferentObj(obj1, obj2) {
-    const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
-    console.log(allKeys);
-    const result = allKeys.map((key) => {
-        const oldValue = obj1[key];
-        const newValue = obj2[key];
-        if (!_.has(obj2, key)) {
-            return {
-                action: 'deleted',
-                key,
-                oldValue
-            };
-        };
-        if (!_.has(obj1, key)) {
-            return {
-                action: 'added',
-                key,
-                newValue
-            };
-        };
-        if (oldValue !== newValue) {
-            return {
-                action: 'changed',
-                key,
-                oldValue,
-                newValue
-            };
-        };
-        return {
-            action: 'unchanged',
-            oldValue,
-            key
-        };
-    });
-    return result;
-};
-
-console.log(genDifferentObj(obj1, obj2));
-
 export { getPath, getData };
+
+// function genDifferentObj(obj1, obj2) {
+//     const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
+//     const result = allKeys.map((key) => {
+//         const oldValue = obj1[key];
+//         const newValue = obj2[key];
+//         if (!_.has(obj2, key)) {
+//             return {
+//                 action: 'deleted',
+//                 key,
+//                 oldValue
+//         }
+//         };
+//         if (!_.has(obj1, key)) {
+//             return {
+//                 action: 'added',
+//                 key,
+//                 newValue
+//             };
+//         };
+//         if (oldValue !== newValue) {
+//             return {
+//                 action: 'changed',
+//                 key,
+//                 oldValue,
+//                 newValue
+//             };
+//         };
+//         return {
+//             action: 'unchanged',
+//             key,
+//             oldValue
+//         };
+//     })
+//     .map((item) => {
+//         const result = [];
+//         if (item.action === 'deleted') {
+//             result.push(`- ${item.key}: ${item.oldValue}`);
+//         }
+//         if (item.action === 'unchanged') {
+//             result.push(`  ${item.key}: ${item.oldValue}`);
+//         }
+//         if (item.action === 'changed') {
+//             result.push(`- ${item.key}: ${item.oldValue}`);
+//             result.push(`+ ${item.key}: ${item.newValue}`);
+//         }
+//         if (item.action === 'added') {
+//             result.push(`+ ${item.key}: ${item.newValue}`);
+//         }
+//         return result;
+//     })
+//     return result.join();
+// };
+
+// console.log(genDifferentObj(obj1, obj2));
+
+// const objects = (genDifferentObj(obj1, obj2));
+
+// const tetsObjects = objects.map((item) => {
+//     const result = [];
+//     if (item.action === 'deleted') {
+//         result.push(`- ${item.key}: ${item.oldValue}`);
+//     }
+//     if (item.action === 'unchanged') {
+//         result.push(`  ${item.key}: ${item.oldValue}`);
+//     }
+//     if (item.action === 'changed') {
+//         result.push(`- ${item.key}: ${item.oldValue}`);
+//         result.push(`+ ${item.key}: ${item.newValue}`);
+//     }
+//     if (item.action === 'added') {
+//         result.push(`+ ${item.key}: ${item.newValue}`);
+//     }
+//     return result;
+// })
+
+// console.log(tetsObjects)
